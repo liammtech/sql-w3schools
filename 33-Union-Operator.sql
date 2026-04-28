81731153deb1ee08da0a26187b3d29bfe94bc1c9
Liam Morrison <liammtech@gmail.com>
2026-04-28 20:58:30 +0100

    ACTUALLY complete lessom 33: Union Operator

--- 33-Union-Operator.sql
+++ 33-Union-Operator.sql
@@ -45,3 +45,46 @@ GO
 -- 1 	        Exotic Liquid 	            Charlotte Cooper 	49 Gilbert St. 	London 	        EC1 4SD 	UK
 -- 2 	        New Orleans Cajun Delights 	Shelley Burke 	    P.O. Box 78934 	New Orleans 	70117 	    USA
 -- 3 	        Grandma Kelly's Homestead 	Regina Murphy 	    707 Oxford Rd. 	Ann Arbor 	    48104 	    USA
+
+
+-- =====
+-- UNION Example
+-- =====
+
+-- Returns the unique (distinct) countries from both the "Customers" and the "Suppliers" table:
+
+SELECT Country FROM Customers
+UNION ALL
+SELECT Country FROM Suppliers
+ORDER BY Country;
+
+-- Note: If some customers or suppliers have the same country, each country will only be listed once, because UNION selects only distinct values. Use UNION ALL to also select duplicate values
+
+
+-- =====
+-- UNION with WHERE
+-- =====
+
+-- Here we ass a WHERE clause to only return the unique German cities from both the tables:
+
+SELECT City, Country FROM Customers
+WHERE Country='Germany'
+UNION
+SELECT City, Country FROM Suppliers
+WHERE Country='Germany'
+ORDER BY City;
+
+
+-- =====
+-- Another UNION Example
+-- =====
+
+-- The following SQL lists all customers and suppliers
+
+SELECT 'Customer' AS Type, ContactName, City, Country
+FROM Customers
+UNION
+SELECT 'Supplier', ContactName, City, Country
+FROM Suppliers;
+
+-- Notice the "AS Type" above - it is an alias. Aliases are used to give a column a temporary name. So, here we have created a temporary column named "Type", that lists whether the contact person is a "Customer" or a "Supplier" (it will factor in the other unionised table names)
