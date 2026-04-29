0a7d45f24c3be853f6178d993ec4fba916123366
liam <liammtech@gmail.com>
2026-04-29 20:58:12 +0100

    Complete lesson 45: Comments

---
+++ 45-Comments.sql
@@ -0,0 +1,19 @@
+IF DB_NAME() <> 'SQL_W3Schools'
+BEGIN
+    USE SQL_W3Schools;
+END
+GO
+
+-- =======================
+-- Comments
+-- =======================
+
+-- Fuck spending time on this, so quick-fire round:
+
+-- (--) for single line comment, goes to end of line
+
+/*
+(/* */) for multi-line comments, goes from first (/*) to terminator (*/)
+*/
+
+-- Access doesn't have comments, because it sucks
